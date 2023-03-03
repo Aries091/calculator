d5218416f4d9cbee49daad5ca8a1a98c4ae38913
Aries091 <97426155+Aries091@users.noreply.github.com>
2023-03-03 15:20:27 +0545

    Update index.js

--- index.js
+++ index.js
@@ -9,22 +9,25 @@ buttons.forEach(function(button){
     button.addEventListener('click',function(e) {
     let value = e.target.dataset.num;
     screen.value+=value;
-
     });
 
 });
+
 equal.addEventListener('click',function(e){
     if(screen.value==''){
         screen.value='';
     }else{
-        let expression = screen.value;
-        if(isNaN(expression[expression.length-1])){
-                expression=expression.slice(0,-1);
-        };
+      let expression = screen.value;
+       //if(isNaN(expression[expression.length-1])){
+       //         expression=expression.slice(0,-1);
+       // };
         let answer =eval(expression);
         screen.value=answer;
+        */
+     console.log(expression)
     }
 })
+
 clear.addEventListener('click',function(e){
   screen.value='';  
 
